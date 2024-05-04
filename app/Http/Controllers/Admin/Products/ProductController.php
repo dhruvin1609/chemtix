e8a27fe281d66cc25509596c4b36b58bac10c628
Dhruvin Patel <pdhruvin1234@gmail.com>
2024-05-04 14:22:50 +0530

    base commit

--- app/Http/Controllers/Admin/Products/ProductController.php
+++ app/Http/Controllers/Admin/Products/ProductController.php
@@ -93,7 +93,7 @@ class ProductController extends Controller
                 $filename = 'product_'.time().'.'.$extension;
                 $file->move(public_path('images/product'), $filename);
                 if($product->image != '' || $product->image != null){
-                    unlink(public_path('images/product'),$product->image);
+                    unlink(public_path('images/product/'.$product->image));
                 }
                 $product->image=$filename;
             }
@@ -124,7 +124,7 @@ class ProductController extends Controller
         if($product == null){
             return response()->json(['status' => false]);
         }
-        unlink(public_path('images/product'),$product->image);
+        unlink(public_path('images/product/'.$product->image));
         $product->delete();
 
         return response()->json(['status' => true]);
